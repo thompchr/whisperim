f0f5048efab50e6f0dd2c0c972a5ec34d961e401
jdlugo <jdlugo@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-01-20 05:40:10 +0000

    1/19: Created branch for John and Kirk's development. -Removed IdleListener class -Updated WhisperClient.java class -Idle status is determined by the last time a user sends a message

---
+++ branches/john/src/org/whisperim/client/AIMStrategy.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package org.whisperim.client;
+
+
+import org.whisperim.aim.*;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author Chris Thompson
+ *
+ * This is the concrete implementation of the ConnectionStrategy class for
+ * an AIM-based chat.
+ */
+public class AIMStrategy implements ConnectionStrategy {
+	
+	AIMSession session;
+	ConnectionManager manager_;
+	
+	
+	public AIMStrategy(ConnectionManager manager){
+
+		manager_ = manager;
+		session = new AIMSession (this);
+		
+	}
+
+
+	@Override
+	public void receiveMessage(Message message) {
+		// TODO Auto-generated method stub
+		manager_.messageReceived(message);
+	}
+
+
+	@Override
+	public void sendMessage(Message message) {
+		session.addOperation(AIMOperation.createSendMessage(message.getTo(), message.getMessage()));
+	}
+
+
+	@Override
+	public void signOff() {
+		
+		session.addOperation(AIMOperation.createSignOut());
+
+	}
+
+
+	@Override
+	public void signOn(String username, String password) {
+
+		session.addOperation(AIMOperation.createSignIn(username, password));
+	}
+	
+	@Override
+	public void receiveBuddies(ArrayList<String> buddies){
+		manager_.receiveBuddies(buddies);
+	}
+
+    @Override
+    public void statusUpdate(String update){
+        manager_.statusUpdate(update);
+    }
+
+}
