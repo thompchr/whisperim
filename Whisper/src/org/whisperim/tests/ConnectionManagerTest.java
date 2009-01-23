377342d5e1a5774a9245f7f00c7627a329d880e7
thompchr <thompchr@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-01-23 23:34:37 +0000

    Added comments to the ConnectionManagerTest class.

--- Whisper/src/org/whisperim/tests/ConnectionManagerTest.java
+++ Whisper/src/org/whisperim/tests/ConnectionManagerTest.java
@@ -8,6 +8,12 @@ import org.whisperim.security.Encryptor;
 
 import junit.framework.TestCase;
 
+/**
+ * This class is designed to provided a set of tests for the ConnectionManager class.
+ * 
+ * @author Nick Krieble
+ *
+ */
 public class ConnectionManagerTest extends TestCase {
 	
 	private static final String test = null;
@@ -19,12 +25,20 @@ public class ConnectionManagerTest extends TestCase {
 		new ConnectionManager(1, test, test2, 
 		testUser, kp.getPrivate(), kp.getPublic());
 	
+	/**
+	 * This method tests the manager's ability to add a connection strategy
+	 * to the hashmap of currently activated protocols.
+	 */
 	public void testAddToMap()
 	{
 		cm.addToMap(test, null);
 		assertTrue(cm.getStrategy(test) != null);
 	}
 	
+	/**
+	 * This method tests the manager's ability to remove connection strategies
+	 * from the hashmap.
+	 */
 	public void testRemoveStrategy()
 	{
 		cm.removeStrategy(test);
