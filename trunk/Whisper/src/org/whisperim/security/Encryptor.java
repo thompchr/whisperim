72aa2acce20c416349ce49be68097df4dc1213c9
thompchr <thompchr@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-02-02 17:11:03 +0000

    Added functionality in Encryptor.java to write a key to a file for storage.  Also corrected method that reads the file.

--- trunk/Whisper/src/org/whisperim/security/Encryptor.java
+++ trunk/Whisper/src/org/whisperim/security/Encryptor.java
@@ -1,4 +1,4 @@
- /**************************************************************************
+/**************************************************************************
  * Copyright 2009 Chris Thompson                                           *
  *                                                                         *
  * Licensed under the Apache License, Version 2.0 (the "License");         *
@@ -17,25 +17,38 @@
 package org.whisperim.security;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.*;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-import org.w3c.dom.*;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 
 /**
@@ -47,11 +60,11 @@ import org.xml.sax.SAXException;
  *
  */
 public class Encryptor {
-	
+
 	private PublicKey publicKey_ = null;
 	private PrivateKey privateKey_ = null;
-	
-	
+
+
 	/**
 	 * Encryptor class constructor.  Requires a PublicKey (foreign) and a PrivateKey(local)
 	 * as parameters.
@@ -61,10 +74,10 @@ public class Encryptor {
 	public Encryptor (PublicKey key, PrivateKey myKey){
 		publicKey_ = key;
 		privateKey_ = myKey;
-			
+
 	}
-	
-	
+
+
 	/**
 	 * Method used for generating an RSA key pair.  This method is 
 	 * static and will only be called when a user first uses the program.
@@ -72,20 +85,20 @@ public class Encryptor {
 	 */
 	public static KeyPair generateRSAKeyPair(){
 		KeyPairGenerator kpg = null;
-		
+
 		try{
 			kpg = KeyPairGenerator.getInstance("RSA");
-			
+
 		}catch(NoSuchAlgorithmException e){
 			//No such algorithm
 			System.err.println(
-            "usage: java AsymmetricKeyMaker <RSA | DSA>");
+			"usage: java AsymmetricKeyMaker <RSA | DSA>");
 
 		}
-		
+
 		kpg.initialize(1024);
 		return kpg.generateKeyPair();
-		
+
 	}
 
 	/**
@@ -96,70 +109,155 @@ public class Encryptor {
 	 * @param handle
 	 * @return PublicKey
 	 */
-    public static PublicKey getPublicKeyForBuddy(String handle){
-        try {
-            File keyFile = new File(System.getProperty("user.home") + File.separator + "Whisper" + File.separator + "keys");
-            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            Document doc;
-            doc = docBuilder.parse(keyFile);
-            // normalize text representation
-            doc.getDocumentElement().normalize();
-            NodeList theirKeys = doc.getElementsByTagName(handle);
+	public static PublicKey getPublicKeyForBuddy(String handle){
+		try {
+			File keyFile = new File(System.getProperty("user.home") + File.separator + "Whisper" + File.separator + "keys");
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc;
+			doc = docBuilder.parse(keyFile);
+			// normalize text representation
+			doc.getDocumentElement().normalize();
+			NodeList buddies = doc.getElementsByTagName("Buddy");
 
-            if (theirKeys.getLength() == 0)
-                //Keys haven't been added
-                return null;
+	
+			Element buddy = null;
+			
+			if (buddies.getLength() != 0){
+				//We need to see if we already have a key for this buddy
+				for (int i = 0; i < buddies.getLength(); ++i){
+					Element temp = (Element)buddies.item(i);
 
-            String keyString = theirKeys.item(0).getFirstChild().getTextContent();
-            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(Base64.decode(keyString.getBytes()));
-            
-    		KeyFactory rsaKeyFac = null;
-    		
-    		try {
-    			rsaKeyFac = KeyFactory.getInstance("RSA");
-    			return rsaKeyFac.generatePublic(pubKeySpec);
-    		} catch (NoSuchAlgorithmException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		} catch (InvalidKeySpecException e) {
+					if (temp.getAttribute(handle) == null){
+						//Not our buddy
+					}else {
+						//We found our buddy
+						buddy = (Element) buddies.item(i);
+
+						break;
+					}
+				}
+			}
+
+			String keyString = buddy.getTextContent();
+			X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(Base64.decode(keyString.getBytes()));
+
+			KeyFactory rsaKeyFac = null;
+
+			try {
+				rsaKeyFac = KeyFactory.getInstance("RSA");
+				return rsaKeyFac.generatePublic(pubKeySpec);
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidKeySpecException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    		
-            return null;
-        } catch (SAXException ex) {
-            Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (Base64DecodingException e) {
+
+			return null;
+		} catch (SAXException ex) {
+			Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (IOException ex) {
+			Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (ParserConfigurationException ex) {
+			Logger.getLogger(Encryptor.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (Base64DecodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        return null;
-    }
-    
-    /**
-     * This method is designed to write a key into the key store file. It takes a string
-     * that has been received from the buddy as well as the buddy's handle.  It will also
-     * verify that the key does not already exist in the file.  If it does, it will ignore
-     * the request.  If the buddy has a different key associated with their buddy handle,
-     * this method will assume the new key is valid and the old key has been discarded.
-     * It will then overwrite the old key.
-     * @param keyText
-     * @param handle
-     */
-    public static void writeKeyToFile(String keyText, String handle){
-    	//Identifier will be handle:protocol
-    	//The handle string passed to the encryptor is already formatted correctly,
-    	//no need to get any more information, just write the bugger.
-    }
-	
+		return null;
+	}
+
+	/**
+	 * This method is designed to write a key into the key store file. It takes a string
+	 * that has been received from the buddy as well as the buddy's handle.  It will also
+	 * verify that the key does not already exist in the file.  If it does, it will ignore
+	 * the request.  If the buddy has a different key associated with their buddy handle,
+	 * this method will assume the new key is valid and the old key has been discarded.
+	 * It will then overwrite the old key.
+	 * @param keyText
+	 * @param handle
+	 */
+	public static void writeKeyToFile(String keyText, String handle){
+		//Identifier will be handle:protocol
+		//The handle string passed to the encryptor is already formatted correctly,
+		//no need to get any more information, just write the bugger.  Key text is
+		//Base64 encoded
+
+		File keyFile = new File(System.getProperty("user.home") + File.separator + "Whisper" + File.separator + "keys");
+		Document doc;
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder;
+		try {
+
+			docBuilder = docBuilderFactory.newDocumentBuilder();
+			doc = docBuilder.parse(keyFile);
+			
+			Node root = doc.getElementsByTagName("Keys").item(0);
+			
+			NodeList buddies = doc.getElementsByTagName("Buddy");
+			
+			Element buddy = null;
+			
+			if (buddies.getLength() != 0){
+				//We need to see if we already have a key for this buddy
+				for (int i = 0; i < buddies.getLength(); ++i){
+					Element temp = (Element)buddies.item(i);
+
+					if (temp.getAttribute(handle) == null){
+						//Not our buddy
+					}else {
+						//We found our buddy
+						buddy = (Element) buddies.item(i);
+
+						break;
+					}
+				}
+			}
+			
+			if (buddy == null){
+				//We didn't find the buddy
+				buddy = doc.createElement("Buddy");
+				buddy.setAttribute("handle", handle);
+				buddy.setTextContent(keyText);
+				
+				
+			}else {
+				buddy.setTextContent(keyText);
+				
+			}
+			
+			root.appendChild(buddy);
+			
+			OutputFormat format = new OutputFormat(doc);
+			format.setIndenting(true);
+
+			XMLSerializer serializer = new XMLSerializer(
+			new FileOutputStream(keyFile), format);
+
+			serializer.serialize(doc);
+
+
+		} catch (ParserConfigurationException e) {
+			
+			e.printStackTrace();
+		} catch (SAXException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+
+	}
+
 	public static KeyPair generateRSAKeyPairFromString(String[] keys) throws Base64DecodingException{
 		KeyFactory rsaKeyFac = null;
-		
+
 		try {
 			rsaKeyFac = KeyFactory.getInstance("RSA");
 		} catch (NoSuchAlgorithmException e) {
@@ -168,14 +266,14 @@ public class Encryptor {
 		}
 		X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(Base64.decode(keys[0].getBytes()));
 		PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(Base64.decode(keys[1].getBytes()));
-		
+
 		PublicKey pubKey = null;
 		PrivateKey privKey = null;
-		
+
 		if (rsaKeyFac != null){
 
 			try {
-				
+
 				pubKey = rsaKeyFac.generatePublic(pubKeySpec);
 				privKey = rsaKeyFac.generatePrivate(privKeySpec);
 
@@ -189,9 +287,9 @@ public class Encryptor {
 		}else{
 			return new KeyPair(pubKey, privKey);
 		}
-		
+
 	}
-	
+
 	/**
 	 * This method is used to generate the encrypted message.  The plaintext is provided as a parameter
 	 * and the ciphertext with the encrypted session key including the <key> and <message> tags is returned.
@@ -201,44 +299,44 @@ public class Encryptor {
 	public String generateCipherText(String message){
 		String encryptedMessage = null;
 		try{
-			
+
 			Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			
+
 			KeyGenerator sessionKeyGen = KeyGenerator.getInstance("AES");
-			
+
 			sessionKeyGen.init(128);
-			
+
 			SecretKey sessionKey = sessionKeyGen.generateKey();
-			
+
 			aesCipher.init(Cipher.ENCRYPT_MODE, sessionKey);
-			
+
 			byte [] byteArray = message.getBytes("UTF-8");
-			
+
 			String cipherMessage = new String (Base64.encode(aesCipher.doFinal(byteArray)));
-			
+
 			encryptedMessage = "<key>";
-			
+
 			Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-			
+
 			rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey_);
-			
+
 			byte [] encryptedKeyBytes = rsaCipher.doFinal(sessionKey.getEncoded());
-			
+
 			String encryptedKeyString = new String (Base64.encode(encryptedKeyBytes));
-			
+
 			encryptedMessage += encryptedKeyString +"</key>" +
-				"<message>" + cipherMessage + "</message>";
+			"<message>" + cipherMessage + "</message>";
 
 
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
-		
+
+
 		return encryptedMessage;
-		
+
 	}
-	
+
 	/**
 	 * This function provides the decryption of an entire message.
 	 * It provides the functionality to initially decrypt the session key
@@ -248,23 +346,23 @@ public class Encryptor {
 	 * @return  String
 	 */
 	public String decryptMessage(String message){
-		
+
 		String outputMessage = null;
 		try {
 
 			String encKey = message.substring(5, message.indexOf("</key>"));
 			byte [] keyBytes = Base64.decode(encKey.getBytes("UTF-8"));
-			
+
 
 			String encMessage = message.substring(message.indexOf("</key>") + 15, message.indexOf("</message>"));
-			
+
 			byte [] messageBytes = Base64.decode(encMessage.getBytes("UTF-8"));
-			
+
 			Cipher rsaCipherDecrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 			rsaCipherDecrypt.init(Cipher.DECRYPT_MODE, privateKey_);
-			
-			
-			
+
+
+
 			Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			SecretKeySpec aesKeyDec = new SecretKeySpec(rsaCipherDecrypt.doFinal(keyBytes), "AES");
 			aesCipher.init(Cipher.DECRYPT_MODE, aesKeyDec);
@@ -274,12 +372,12 @@ public class Encryptor {
 		}catch (Exception e) {
 			e.printStackTrace();
 		} 
-		
+
 		return outputMessage;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Set method for the publicKey_ member variable.
 	 * @param key
@@ -287,7 +385,7 @@ public class Encryptor {
 	public void setPublicKey(PublicKey key){
 		publicKey_ = key;
 	}
-	
+
 	/**
 	 * Set method for the privateKey_ member variable.
 	 * @param key
@@ -295,6 +393,6 @@ public class Encryptor {
 	public void setPrivateKey(PrivateKey key){
 		privateKey_ = key;
 	}
-	
-	
+
+
 }
