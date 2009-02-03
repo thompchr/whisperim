c413ee30ddc24a3a77b52dfc5bb02589a4c4f961
nKrieble <nKrieble@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-02-03 04:31:43 +0000

    Comments added.

--- Whisper/src/org/whisperim/client/ConnectionManager.java
+++ Whisper/src/org/whisperim/client/ConnectionManager.java
@@ -30,6 +30,10 @@ import java.util.HashMap;
  */
 public class ConnectionManager {
 
+	/*
+	 * This section defines private member variables. 
+	 * Each title holds data for the session connection.
+	 */
 	private ConnectionStrategy strategy_;
 	private String localHandle_;
 	private Login login_;
@@ -40,18 +44,32 @@ public class ConnectionManager {
 
 	public static final int AIM_SESSION = 0; 
 
+	/*
+	 * Hashmap strategies_ holds session strategies.
+	 */
 	private HashMap<String, ConnectionStrategy> strategies_ = new HashMap<String, ConnectionStrategy>();
 
+	/*
+	 * Returns true if Hashmap strategies_ is empty.
+	 */
 	public boolean isHashEmpty()
 	{
 		return (strategies_.isEmpty());
 	}
 	
+	/*
+	 * Returns strategies from Hashmap strategies_.
+	 * @param strategy is the strategy title to be retrieved from strategies_. 
+	 */
 	public ConnectionStrategy getStrategy(String strategy)
 	{
 		return strategies_.get(strategy);
 	}
 
+	/*
+	 * Removes strategies from Hashmap strategies_.
+	 * @param strategy is the strategy title to be removed from strategies_. 
+	 */
 	public void removeStrategy(String strategy)
 	{
 		if(!strategy.isEmpty())
@@ -62,21 +80,35 @@ public class ConnectionManager {
 		}
 	}
 
+	/*
+	 * Adds strategies to Hashmap strategies_.
+	 * @param strategy is the strategy title to be added to strategies_.
+	 * @param is the key for strategies_. 
+	 */
 	public void addToMap(String name, ConnectionStrategy strategy)
 	{
 		strategies_.put(name, strategy);
 	}
 
+	/*
+	 * Constructor for Connection Manager.
+	 */
 	public ConnectionManager(int strategy, String handle, String password, Login login, PrivateKey privKey, PublicKey pubKey) throws IllegalArgumentException{
 		if (handle == null){
 			throw new IllegalArgumentException();
 		}
 
 
+		// Switch statement for strategies. 
 		switch(strategy){
-		case 0: {
-			strategy_ = new AIMStrategy(this);
-		}
+			case 0: 
+			{
+				System.out.println("Error: No strategy found!");
+			}
+			case 1:
+			{
+				strategy_ = new AIMStrategy(this);
+			}
 		}
 
 		myPrivateKey_ = privKey;
@@ -87,49 +119,83 @@ public class ConnectionManager {
 
 	}
 
+	/*
+	 * Returns strategy_.
+	 */
 	public ConnectionStrategy getStrategy(){
 		return strategy_;
 	}
 
+	/*
+	 * Set client_ to specified chat client
+	 * @param client is the chat client. 
+	 */
 	public void setClient(WhisperClient client){
 		client_ = client;
 	}
 
+	/*
+	 * Return localHandle_.
+	 */
 	public String getHandle(){
 		return localHandle_;
 	}
 
+	/*
+	 * Action taken when message is received from client.
+	 */
 	public void messageReceived(Message message){
 		System.out.println("Received message: " + message.getMessage());
 		client_.recieveMessage(message);
 
 	}
 
+	/*
+	 * Updates status.
+	 */
 	public void statusUpdate (String status){
 		login_.statusUpdate(status);
 	}
 
+	/*
+	 * Updates buddy list when buddy shows up.
+	 */
 	public void receiveBuddies(ArrayList<String> buddies){
 		client_.updateBuddyList(buddies);
 	}
 
+	/*
+	 * Action to send message.
+	 */
 	public void sendMessage(Message message){
 		System.out.println("Sending message: " + message.getMessage());
 		strategy_.sendMessage(message);
 	}
-
+	
+	/*
+	 * Sign off strategy.
+	 */
 	public void signOff(){
 		strategy_.signOff();
 	}
 
+	/*
+	 * Get Private key for encryption.
+	 */
 	public PrivateKey getPrivateKey(){
 		return myPrivateKey_;
 	}
 
+	/*
+	 *  Get public key for encryption.
+	 */
 	public PublicKey getPublicKey(){
 		return myPublicKey_;
 	}
 
+	/*
+	 *  Set status to away.
+	 */
 	 public void setAwayMessage(String message, boolean away){
 		 strategy_.setAwayMessage(message, away);
 	 }
