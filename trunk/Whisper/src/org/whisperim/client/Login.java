24f8b5b15da57c911172d591fab115d79bda1ff1
jplastek <jplastek@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-02-18 23:42:01 +0000

    Lots of minor changes, modified Login, WhisperClient, WhisperNewIM and WhisperPrefs.

--- trunk/Whisper/src/org/whisperim/client/Login.java
+++ trunk/Whisper/src/org/whisperim/client/Login.java
@@ -18,10 +18,24 @@ package org.whisperim.client;
 
 
 import java.awt.EventQueue;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import javax.swing.*;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+
 
 
 /**
