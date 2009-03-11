b5c5d3efca624739103ad68246eff7d5a1df0d98
nKrieble <nKrieble@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-03-11 02:58:06 +0000

    retry for context and state of look and feel

--- trunk/Whisper/src/org/whisperim/client/LookAndFeelState.java
+++ trunk/Whisper/src/org/whisperim/client/LookAndFeelState.java
@@ -1,5 +1,5 @@
 /**************************************************************************
- * Copyright 2009 Cory Plastek                                             *
+ * Copyright 2009 Nick Krieble                                             *
  *                                                                         *
  * Licensed under the Apache License, Version 2.0 (the "License");         *
  * you may not use this file except in compliance with the License.        *
@@ -17,5 +17,6 @@
 package org.whisperim.client;
 
 public interface LookAndFeelState {
-
+	public void setState(StateContext ctx);
+	public String getState();
 }
