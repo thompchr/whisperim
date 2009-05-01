f6c4633aa746160e151f48389d5da41eddcb987f
jplastek <jplastek@0cdd8170-de61-11dd-87d8-31aa04531ed5>
2009-05-01 02:41:41 +0000

    yahoo

---
+++ branches/cory/src/org/whisperim/yahoo/YahooConnector.java
@@ -0,0 +1,93 @@
+package org.whisperim.yahoo;
+
+import javax.swing.ImageIcon;
+
+import org.whisperim.plugins.ConnectionPluginAdapter;
+
+public class YahooConnector extends ConnectionPluginAdapter implements ConnectionStrategy {
+
+	@Override
+	public String getHandle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProtocol() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageIcon getServiceIcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageIcon getIcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPluginIconLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPluginName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setIconLocation(String location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPluginName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getStatus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHandle(String handle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIdle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setInvisible(boolean visible) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStatusMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	{
+
+}
